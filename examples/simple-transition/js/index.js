69bbeddad631d79109b7a73a3103df2bbd8d8a0e
Rutger Bakker <r.bakker@nerds.company>
2017-02-16 16:21:20 +0100

    page transitions in progress

---
+++ examples/simple-transition/js/index.js
@@ -0,0 +1,14 @@
+/**
+ * Turbolinks Transitions
+ * Copyright 2017 Nerds & Company
+ */
+$(() => {
+    $(document).on('click', 'a[href]', (e) => {
+        e.preventDefault();
+        const url = e.target.href;
+        $('.js-animation').removeClass('is-entering').addClass('is-leaving').one('webkitAnimationEnd mozAnimationEnd animationEnd', () => {
+            Turbolinks.visit(url);
+            $('.js-animation').removeClass('is-leaving').addClass('is-entering');
+        });
+    });
+});
