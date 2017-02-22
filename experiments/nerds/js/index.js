59b5c7910483ede1519d739e0b58045d0bb91acd
Sander Tijink <s.tijink@nerds.company>
2017-02-22 16:15:39 +0100

    Particles en telescopic text

---
+++ experiments/nerds/js/index.js
@@ -0,0 +1,40 @@
+/**
+ * Turbolinks Transitions
+ * Copyright 2017 Nerds & Company
+ */
+$(() => {
+    $(document).on('click', 'a[href]', (e) => {
+        e.preventDefault();
+        $('.image').removeClass('active');
+        $(e.target).addClass('active');
+        $("body, html").animate({
+          scrollTop: $(e.target).position().top
+        });
+        const url = e.target.href;
+        $('.js-animation').removeClass('is-entering').addClass('is-leaving').one('webkitAnimationEnd mozAnimationEnd animationEnd', () => {
+            Turbolinks.visit(url);
+            $('.js-animation').removeClass('is-leaving').addClass('is-entering');
+        });
+    });
+});
+
+
+// Telescopic text
+$(function(){
+// attach event handlers to all expanders
+    $('.expander').bind('click', function () {
+    // find all sections with class name
+    // corresponding to expander's id
+    // that haven't been expanded yet
+    var sections = $('.' + this.id + ':not(.expanded)');
+    // if there's only one section left
+    // and we are doing our "last" expanding for this section
+    // remove "onclick" handler from this expander
+    // and stop highligting it (remove CSS class "expander")
+    if ( sections.length <= 1 ) {
+        $(this).removeClass('expander').unbind('click');
+    }
+    // expand first section
+    sections.first().addClass('expanded');
+    });
+});
